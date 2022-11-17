f6f4cf8d8900a45933777963b172d44812c0404b
Alessia B <borys.alessia@gmail.com>
2022-11-17 16:33:54 +0100

    extended file tree

---
+++ src/components/MyProjects.tsx
@@ -0,0 +1,6 @@
+export default function MyProjects(): JSX.Element {
+    return (
+        <>
+        </>
+    )
+}
---
+++ src/components/about-me/education.tsx
@@ -0,0 +1,6 @@
+export default function Education(): JSX.Element {
+    return (
+        <>
+        </>
+    )
+}
