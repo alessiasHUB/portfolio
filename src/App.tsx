f4aecb20808e6b09a5adb616240d90f4089f2c48
Alessia B <borys.alessia@gmail.com>
2022-11-17 16:28:58 +0100

    page tree set up

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PageFooter from "./components/PageFooter";
 import PageHeader from "./components/PageHeader";
 
 function App(): JSX.Element {
+
   return (
     <>
       <PageHeader />
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,13 @@
+import Skills from "./about-me/skills"
+import Education from "./about-me/education"
+import Hobbies from "./about-me/hobbies"
+
+export default function AboutMe(): JSX.Element {
+    return (
+        <>
+        <Education />
+        <Skills />
+        <Hobbies />
+        </>
+    )
+}
