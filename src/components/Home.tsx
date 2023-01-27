0551bf2626236f1460e59dc5633f8824bb575b64
Alessia B <borys.alessia@gmail.com>
2023-01-27 11:22:35 +0100

    style and background

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,6 @@ const Home: React.FunctionComponent<IHomeProps> = (props) => {
   return (
     <main className="content">
       <div className="home">
-        <p>Hi, My name is Alessia.</p>
       </div>
     </main>
   );
