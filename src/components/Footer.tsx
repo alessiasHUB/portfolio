a74214bf836bf1c56b8939f347877ae0456570d3
Alessia B <borys.alessia@gmail.com>
2023-02-03 16:56:30 +0100

    basic style done on landing page, needs images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faFaceSmile } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import "./footer.css";
+import contact from "../imgs/contact.png"
+import about from "../imgs/about.png"
 
 function Footer(): JSX.Element {
   return (
     <div className="footer-container">
       <div className="container__row">
         <div className="about-container">
-          <h1>About</h1>
+        <img src={about} alt=""/>
           <div className="about-section">
             <FontAwesomeIcon icon={faFaceSmile} className="portrait-pic" />
             {/* <img src="#" alt="" /> */}
@@ -16,7 +18,7 @@ function Footer(): JSX.Element {
           </div>
         </div>
         <div className="contact-container">
-          <h1>Contact</h1>
+          <img src={contact} alt=""/>
           <p>
             If you have any questions about anything, or just want to say hi,
             feel free to contact me. You can find my social pages and email
