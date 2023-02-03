f90bcf2720540a4c3b0f63f826f3c1e9a5aa91e0
Alessia B <borys.alessia@gmail.com>
2023-02-03 15:51:50 +0100

    commit for pulling images

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import "./landingPage.css";
 
 export default function LandingPage(): JSX.Element {
   return (
     <>
       <Header />
       <div className="interactive-resume-div">
-      <Link className="nav-text" to="/interactive-resume">
-        Interactive resume
-      </Link>
+        <h1>INTERACTIVE RESUME</h1>
+        <p>
+          Let's go over my resume, but make it a little more fun with this
+          interactive game style website
+        </p>
+        <button className="interactive-resume-btn">
+          <Link className="interactive-resume-link" to="/interactive-resume">
+            LAUNCH
+          </Link>
+        </button>
       </div>
       <Footer />
     </>
