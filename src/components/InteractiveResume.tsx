2fdcac5c0fe91c124b9d5bf2b26c262548fb5b6e
Alessia B <borys.alessia@gmail.com>
2023-02-03 16:24:53 +0100

    helpful links inserted

--- src/components/InteractiveResume.tsx
+++ src/components/InteractiveResume.tsx
@@ -2,6 +2,9 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
+// link to one page links, scroll
+// https://medium.com/geekculture/scrollable-single-page-site-navigation-with-react-custom-hooks-4e7af716f6b1
+
 export default function InteractiveResume(): JSX.Element {
   return (
     <>
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import "./landingPage.css";
+import interactresText from "../imgs/interactiveresumetxt.png"
 
 export default function LandingPage(): JSX.Element {
+  const intertxtHeight= 40
   return (
     <>
       <Header />
       <div className="interactive-resume-div">
-        <h1>INTERACTIVE RESUME</h1>
+    <img src={interactresText} alt="txt" height={intertxtHeight}/>
         <p>
           Let's go over my resume, but make it a little more fun with this
           interactive game style website
